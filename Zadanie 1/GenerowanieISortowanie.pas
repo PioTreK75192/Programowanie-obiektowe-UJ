08fd8ae76332dd4ed1cca8eea71260c98d342d64
PioTreK75192 <piotrek07075@proton.me>
2026-03-25 20:21:05 +0100

    Procedura do sortowania liczb

--- Zadanie 1/GenerowanieISortowanie.pas
+++ Zadanie 1/GenerowanieISortowanie.pas
@@ -19,6 +19,21 @@ begin
     t[i] := random(101);
 end;
 
+{ Procedura sortująca }
+procedure Sortuj(var t: Tablica);
+var
+  i, j, temp: integer;
+begin
+  for i := 1 to N - 1 do
+    for j := 1 to N - i do
+      if t[j] > t[j + 1] then
+      begin
+        temp := t[j];
+        t[j] := t[j + 1];
+        t[j + 1] := temp;
+      end;
+end;
+
 { Procedura wypisująca tablicę }
 procedure Wypisz(t: Tablica);
 var
@@ -32,5 +47,9 @@ end;
 begin
   Generuj(liczby);
   writeln('Wylosowane liczby:');
+  Wypisz(liczby);
+
+  Sortuj(liczby);
+  writeln('Po sortowaniu:');
   Wypisz(liczby);
 end.
